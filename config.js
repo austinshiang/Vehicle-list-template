dd81d3b101bb4621f8dd6d2472ca0cbee675dcb8
austinshiang <shiang340@gmail.com>
2026-08-10 17:00:12 +0800

    config.js

--- config.js
+++ config.js
@@ -4,25 +4,25 @@
  */
 window.SHOP_CONFIG = {
     // 1. 車行名稱（顯示於網頁標題與頂端 Header）
-    shopName: "豆豆豆豆包車業",
+    shopName: "模範車業",
     
     // 2. Google Apps Script 資料庫 API 網址
     apiUrl: "https://script.google.com/macros/s/AKfycbw0GFMdyUxeOt-iiGzRNJOK6AudfOMD7DkUTcr_1z65YGhsoymaJjdTEYXKIJOrMbKa/exec",
     
     // 3. 主要品牌 Logo 圖片網址
-    logoUrl: "https://storage.moegirl.org.cn/moegirl/commons/0/0d/%E8%B1%86%E5%8C%85AI.png",
+    logoUrl: "./logo1.png",
     
     // 4. 鎖屏認證頁面的輪播 Logo 網址（可設定 1~3 張）
     lockLogos: [
-        "https://storage.moegirl.org.cn/moegirl/commons/0/0d/%E8%B1%86%E5%8C%85AI.png"
+        "./logo1.png"
     ],
     
     // 5. 背景浮水印圖片網址
     watermarkUrl: "https://images.1111.com.tw/oad/117197616.jpg",
     
     // 6. 頁尾顯示版權宣告文字
-    footerText: "© 姜駒車業. All Rights Reserved. 整合系統 v5.7",
+    footerText: "© 賺錢車業. All Rights Reserved. 整合系統 v5.7",
 
     // 7. 預設解鎖密碼（當 Google 試算表中沒有設定 CONFIG_PASSWORD 時使用）
-    defaultPasscode: "8888"
+    defaultPasscode: "168888"
 };
